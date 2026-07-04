364caf7f9755e53164b5226040e8aaee86e10a13
Andezion <245122@edu.p.lodz.pl>
2026-07-04 15:02:44 +0200

    add: added new model

---
+++ lib/models/app_state.dart
@@ -0,0 +1,119 @@
+import 'session.dart';
+
+class Bodyweight {
+  const Bodyweight({required this.date, required this.kg});
+
+  final String date;
+  final double kg;
+
+  factory Bodyweight.fromJson(Map<String, dynamic> j) => Bodyweight(
+        date: j['date'] as String,
+        kg: (j['kg'] as num).toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {'date': date, 'kg': kg};
+}
+
+class Goal {
+  const Goal({
+    required this.id,
+    required this.exId,
+    required this.arm,
+    required this.target,
+  });
+
+  final String id;
+  final String exId;
+  final String arm; 
+  final double target;
+
+  factory Goal.fromJson(Map<String, dynamic> j) => Goal(
+        id: j['id'] as String,
+        exId: j['exId'] as String,
+        arm: j['arm'] as String,
+        target: (j['target'] as num).toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'exId': exId,
+        'arm': arm,
+        'target': target,
+      };
+}
+
+class AppState {
+  const AppState({
+    required this.name,
+    required this.cursor,
+    required this.sessions,
+    required this.bw,
+    required this.goals,
+    required this.tourney,
+    required this.demo,
+  });
+
+  final String name;
+  final int cursor;
+  final List<Session> sessions;
+  final List<Bodyweight> bw;
+  final List<Goal> goals;
+  final String tourney;
+  final bool demo;
+
+  factory AppState.fromJson(Map<String, dynamic> j) => AppState(
+        name: (j['name'] as String?) ?? '',
+        cursor: (j['cursor'] as int?) ?? 0,
+        sessions: ((j['sessions'] as List?) ?? const [])
+            .map((e) => Session.fromJson(e as Map<String, dynamic>))
+            .toList(),
+        bw: ((j['bw'] as List?) ?? const [])
+            .map((e) => Bodyweight.fromJson(e as Map<String, dynamic>))
+            .toList(),
+        goals: ((j['goals'] as List?) ?? const [])
+            .map((e) => Goal.fromJson(e as Map<String, dynamic>))
+            .toList(),
+        tourney: (j['tourney'] as String?) ?? '',
+        demo: (j['demo'] as bool?) ?? false,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'cursor': cursor,
+        'sessions': sessions.map((s) => s.toJson()).toList(),
+        'bw': bw.map((b) => b.toJson()).toList(),
+        'goals': goals.map((g) => g.toJson()).toList(),
+        'tourney': tourney,
+        'demo': demo,
+      };
+
+  AppState copyWith({
+    String? name,
+    int? cursor,
+    List<Session>? sessions,
+    List<Bodyweight>? bw,
+    List<Goal>? goals,
+    String? tourney,
+    bool? demo,
+  }) {
+    return AppState(
+      name: name ?? this.name,
+      cursor: cursor ?? this.cursor,
+      sessions: sessions ?? this.sessions,
+      bw: bw ?? this.bw,
+      goals: goals ?? this.goals,
+      tourney: tourney ?? this.tourney,
+      demo: demo ?? this.demo,
+    );
+  }
+
+  static const empty = AppState(
+    name: '',
+    cursor: 0,
+    sessions: [],
+    bw: [],
+    goals: [],
+    tourney: '',
+    demo: false,
+  );
+}
