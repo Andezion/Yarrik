12bd93fbd730c93574c5d68173da1446e79ef93e
Andezion <245122@edu.p.lodz.pl>
2026-07-04 15:05:17 +0200

    add: added new model

---
+++ lib/models/requests.dart
@@ -0,0 +1,69 @@
+library;
+
+class CreateSetRequest {
+  const CreateSetRequest({required this.arm, required this.weight, required this.reps});
+
+  final String arm;
+  final dynamic weight; 
+  final dynamic reps;
+
+  Map<String, dynamic> toJson() => {'arm': arm, 'weight': weight, 'reps': reps};
+}
+
+class CreateEntryRequest {
+  const CreateEntryRequest({required this.exId, required this.sets});
+
+  final String exId;
+  final List<CreateSetRequest> sets;
+
+  Map<String, dynamic> toJson() => {
+        'exId': exId,
+        'sets': sets.map((s) => s.toJson()).toList(),
+      };
+}
+
+class CreateSessionRequest {
+  const CreateSessionRequest({
+    required this.date,
+    required this.workoutIdx,
+    required this.entries,
+    this.duration,
+    this.mood,
+    this.fatigue,
+    this.rpe,
+    this.notes = '',
+    this.tags = const [],
+  });
+
+  final String date;
+  final int workoutIdx;
+  final int? duration;
+  final int? mood;
+  final int? fatigue;
+  final int? rpe;
+  final String notes;
+  final List<String> tags;
+  final List<CreateEntryRequest> entries;
+
+  Map<String, dynamic> toJson() => {
+        'date': date,
+        'workoutIdx': workoutIdx,
+        'duration': duration,
+        'mood': mood,
+        'fatigue': fatigue,
+        'rpe': rpe,
+        'notes': notes,
+        'tags': tags,
+        'entries': entries.map((e) => e.toJson()).toList(),
+      };
+}
+
+class CreateGoalRequest {
+  const CreateGoalRequest({required this.exId, required this.arm, required this.target});
+
+  final String exId;
+  final String arm;
+  final double target;
+
+  Map<String, dynamic> toJson() => {'exId': exId, 'arm': arm, 'target': target};
+}
